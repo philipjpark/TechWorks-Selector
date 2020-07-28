a0ce0e0ed0c471eb975c148c3682bc836c347b5d
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-07-28 18:34:12 -0400

    selector nearly complete sans edges

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Shape from "./Shape";
+
+class Selector extends Component {
+   constructor() {
+       super();
+       this.state = {
+           selectedShape: 'square'
+       };
+   };
+
+   selectShape = (shapeName) => {
+    this.setState({
+        selectedShape: shapeName
+    })
+   }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="navbar">
+          <div> Selected:</div>
+          <div> {this.state.selectedShape}</div>
+        </div>
+        <div className="shape-list">
+          <Shape shape="square" selectShape ={this.selectShape} />
+          <Shape shape="circle" selectShape ={this.selectShape} />
+          <Shape shape="triangle" selectShape ={this.selectShape}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Selector;
---
+++ src/Shape.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Shape = (props) => {
+  const shape = props.shape;
+  const selectShape = props.selectShape;
+  return <div className={shape} onClick={() => selectShape(shape)} />;
+};
+
+export default Shape;
